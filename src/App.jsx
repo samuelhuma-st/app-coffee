1b55f93a676366f2fa6095f1cf3a958a4be4e97e
samuelhuma-st <83666935+samuelhuma-st@users.noreply.github.com>
2021-12-30 19:37:25 +0100

    background

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const messageCommande = React.createRef();
+
+  const [stateActiveBlock, setStateActiveBlock] = useState(true);
+
+  const block = ["Fill"];
+
+  const activeCommande = () => {
+    setStateActiveBlock((prevState) => !prevState);
+    console.log(messageCommande.current);
+  };
+
+  useEffect(() => {
+    document.title = "Crème glacé";
+  });
+  return (
+    <>
+      <div className="area" style={{ maxWidth: "450px", height: "400px" }}>
+        <div
+          className=""
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <h3 className="h3">Crème glacé</h3>
+          <button className="buttonAddCommande" onClick={activeCommande}>
+            +
+          </button>
+        </div>
+        <div
+          className="backgroundCoffee"
+          style={{ maxWidth: "450px", height: "80%" }}
+        >
+          <div
+            ref={messageCommande}
+            className="alertNoCommande"
+            style={{ display: `${stateActiveBlock ? "block" : "none"}` }}
+          >
+            Aucune commande
+          </div>
+          <div>{stateActiveBlock ? block : "Call"}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
